5c6104b745166933c86a5f8ea5d74f7b66d68207
Kunal Chandan <kunal_chandan@outlook.com>
2019-10-23 14:09:15 -0400

    Resolve bad Lines

--- FileReader/sql2csv.py
+++ FileReader/sql2csv.py
@@ -5,15 +5,23 @@ def sql2csv(f_name):
     f = open(f_name, 'r+')
     data = []
     # Read header
-    head = [a.strip(' ') for a in f.readline().strip('\n').split('|')]
+    head = [a for a in f.readline().split('|')]
     # Drop linebreak
     linebreak = [a.strip(' ') for a in f.readline().strip('\n')]
     data.append(linebreak) if len(linebreak) == len(head) else print("Beginning processing")
     for line in f:
-        l = [a.strip(' ') for a in line.strip('\n').split('|')]
-        if len(l) != len(head):
-            continue
-        else:
-            data.append(l)
+        start = 0
+        row = []
+        for column in head:
+            l = line[start:start+len(column)]
+            if len(l) == 0:
+                print("Dropping line::", line, l, column)
+                continue
+            start += len(column) + 1
+            row.append(l.strip('\n').strip(' '))
+        if len(row) == len(head):
+            data.append(row)
     f.close()
+    head = [a.strip('\n').strip(' ') for a in head]
+    print("End processing")
     pd.DataFrame(data, columns=head, index=None).to_csv(f_name + '.csv')
